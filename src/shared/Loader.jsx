fc4b4ec231aef37c6a48b8918344e0e0ed917315
devsilva <jerson.silva99@hotmail.com>
2023-01-27 22:40:28 -0500

    feat: Create loader bar component

---
+++ src/shared/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+    return <>
+        <div className='load__bar'>
+            <div className='bar' />
+            <div className='bar' />
+            <div className='bar' />
+        </div>
+    </>
+}
+
+export default Loader
