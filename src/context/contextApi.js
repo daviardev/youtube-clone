1ac3cb1171a3d244744cc6fe6422445302fa46c4
devsilva <jerson.silva99@hotmail.com>
2023-01-27 20:53:20 -0500

    feat: Get search from api

---
+++ src/context/contextApi.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, createContext } from 'react'
+
+import { fetchDataFromApi } from 'utils/api'
+
+export const Context = createContext()
+
+const AppContext = props => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const [searchResults, setSearchResults] = useState(false)
+    const [selectCategories, setSelectCategories] = useState(false)
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectCategories)
+    }, [selectCategories])
+
+    const fetchSelectedCategoryData = query => {
+        setIsLoading(true)
+        fetchDataFromApi(`search/?q=${query}`).then(res => {
+            console.log(res)
+            // setSearchResults(res)
+            setIsLoading(false)
+        })
+    }
+
+    return <>
+        <Context.Provider value={{
+            isLoading,
+            setIsLoading,
+            searchResults,
+            setSelectCategories,
+            mobileMenu,
+            setMobileMenu
+        }}>
+            {props.children}
+        </Context.Provider>
+    </>
+}
+
+export default AppContext
