b550c4cf021e308eb41c491a0f1ddb5d00e7b8e9
devsilva <jerson.silva99@hotmail.com>
2023-01-28 20:45:35 -0500

    refactor: Style like youtube

--- src/shared/VideoLength.jsx
+++ src/shared/VideoLength.jsx
@@ -7,7 +7,7 @@ const VideoLength = ({ time }) => {
         ?.format('H:mm:ss')
 
     return <>
-        <span className='absolute bottom-2 right-2 bg-black py-1 text-white text-xs rounded-md'>
+        <span className='absolute bottom-2 right-2 bg-black py-1 text-white text-xs rounded w-[3rem] text-center opacity-[85%]'>
             { videoLengthSecs }
         </span>
     </>
